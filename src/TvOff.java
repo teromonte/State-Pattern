a1892aa3116f6985ac75be83951378227fcb0b96
Tiago <tl.vieira@campus.fct.unl.pt>
2021-10-25 23:28:40 +0100

    first version

--- src/TvOff.java
+++ src/TvOff.java
@@ -1,2 +1,20 @@
-public class TvOff {
+public class TvOff implements Tv{
+    public TvOff() {
+
+    }
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public void changeChannel(int channel) {
+
+    }
 }
