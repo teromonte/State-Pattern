99b1243746b39200970ce334cddd71bddd46e7ef
Tiago <tl.vieira@campus.fct.unl.pt>
2021-10-26 22:15:47 +0100

    prints main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         StateContext context = new StateContext();
-        context.lookAtLamp("name");
-        context.lookAtLamp("name");
+        System.out.println(context.lookAtLamp());
+        System.out.println(context.lookAtLamp());
     }
 }
